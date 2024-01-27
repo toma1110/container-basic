3d47aa7a271edf417a597679733727f82ea94149
toma-shohei <toma.shohei@tis.co.jp>
2024-01-27 15:15:15 +0900

    modify

--- lib/stack/ecs-stack.ts
+++ lib/stack/ecs-stack.ts
@@ -35,10 +35,10 @@ export class EcsStack extends Stack {
         // VPCEndpoint  ※高額なため作業が日をまたぐ場合は一度コメントアウトし、再開時にコメントインする
         new VpcEndpoint(this, this.securityGroup);
 
-        // // ECS ※targetGroupが無いとエラーになる。　※高額なため作業が日をまたぐ場合は一度コメントアウトし、再開時にコメントインする
+        // // ECS 「targetGroupが無いとエラーになる。」　※高額なため作業が日をまたぐ場合は一度コメントアウトし、再開時にコメントインする
         // const ecs = new Ecs(this, vpcStack.vpc, Stack.of(this).account, this.securityGroup, targetGroup, loadBalancer);
 
-        // // 「コンテナのリリース（ブルーグリーンデプロイ）を体験」実行の際にコメントイン
+        // // CodeDeploy「targetGroupが無いとエラーになる。」
         // const codedeploy = new CodeDeploy(this, this.securityGroup);
 
 
